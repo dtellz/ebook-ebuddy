ed55ae9d96940646ea93866be9950059dd6adaa4
dtellz <dtellez95@gmail.com>
2023-07-09 21:05:01 +0100

    setup server startup process without venv activation via yarn

--- server/main.py
+++ server/main.py
@@ -1,10 +1,9 @@
 import time
 from flask import Flask
-from  api.test import run
+from  api.epub_loader import loadEpub
 
 app = Flask(__name__)
 
 @app.route('/time')
 def get_current_time():
-    run()
     return {'time': time.time()}
